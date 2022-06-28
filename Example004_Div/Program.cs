1daf3c286ba6a1239820dcab592e1db29da50a1b
Valentino <ghost085@mail.ru>
2022-06-28 23:06:19 +0300

    add Random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double nuberA=12;
-double numberB=5;
-Console.WriteLine(nuberA/numberB);
+﻿double numberA=new Random().Next(1, 10); // 1, 2, 3, ... 9
+Console.WriteLine(numberA);
+double numberB=new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA/numberB);
