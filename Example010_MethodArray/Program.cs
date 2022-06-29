a8e78bb1fea3a7c2ea090aaf013df051cc101913
Valentino <ghost085@mail.ru>
2022-06-29 21:19:28 +0300

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int[] array = { 15, 21, 39, 12, 23, 33, 11, 17 };
+
+int n = array.Length;
+
+int find = 23;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
