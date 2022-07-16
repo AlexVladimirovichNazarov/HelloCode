530b2545eb36ae7c1d4b3cc27c331c409e5f87aa
Valentino <ghost085@mail.ru>
2022-07-16 17:24:30 +0300

    Example013_RecursionsAlgarithm

---
+++ Example013_RecursionsAlgarithm/Program.cs
@@ -0,0 +1,136 @@
+﻿// 1 - словами, 2 - Заполняем 0, т.к. ничем не заполняли, 3 - заполняем случайными числами.
+
+// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//    for (int colums = 0; colums < 5; colums++)
+//    {
+//      Console.WriteLine($"-{table[rows, colums]}-");
+//    }
+// }
+
+void PrintArray(int[,] matr)
+{
+    // for (int rows = 0; rows < 3; rows ++)
+    for (int rows = 0; rows < matr.GetLength(0); rows++)  // matrix.GetLength(0) - строки
+    {
+        // for (int colums = 0; colums < 4; colums ++)
+        for (int colums = 0; colums < matr.GetLength(1); colums++) // matrix.GetLength(1) - столбцы
+        {
+            Console.Write($"{matr[rows, colums]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)  // matrix.GetLength(0) - строки
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // matrix.GetLength(1) - столбцы
+        {
+            matr[i, j] = new Random().Next(1, 10); // [1, 10)
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+
+Console.WriteLine();
+
+FillArray(matrix);
+PrintArray(matrix);
+
+
+int[,] pic = new int[23, 25]
+{
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+void PrintImage(int[,] image) // Печатаем двумерный массив
+{
+    for (int i = 0; i < image.GetLength(0); i++)  // matrix.GetLength(0) - строки
+    {
+        for (int j = 0; j < image.GetLength(1); j++) // matrix.GetLength(1) - столбцы
+        {
+            if (image[i, j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImage(int row, int col) // Закрашиваем внутренности
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col); // Рекурсия
+        FillImage(row, col - 1); // Рекурсия
+        FillImage(row + 1, col); // Рекурсия
+        FillImage(row, col + 1); // Рекурсия
+    }
+}
+
+PrintImage(pic);
+
+FillImage(13, 13);
+PrintImage(pic);
+
+// Факториал
+
+double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 20; i++)
+{
+Console.WriteLine($"Factorial({i})! = {Factorial(i)}");
+}
+
+Console.WriteLine();
+
+// Фибоначи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1; // || - обозначает или
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"Fibonacci({i}) = {Fibonacci(i)}");
+}
+
